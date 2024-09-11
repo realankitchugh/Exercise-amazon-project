704df4277292d94d115c4376cb47cea38d95b4b6
realankitchugh <ankitchughpro@gmail.com>
2024-09-11 23:48:44 +0530

    Use product.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,32 +1,3 @@
-products=[
-  {
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    rating: {
-      stars: 4.5,
-      count: 87
-    },
-    priceCents: 1090,
-  },
-  {
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    rating: {
-      stars: 4.5,
-      count: 56
-    },
-    priceCents: 799,
-  },
-  {
-    name: 'Intermediate Size Basketball',
-    image: 'images/products/intermediate-composite-basketball.jpg',
-    rating: {
-      stars: 4,
-      count: 127
-    },
-    priceCents: 2095,
-  }
-]
 let html='';
 products.forEach((product) => {
   html+=`
