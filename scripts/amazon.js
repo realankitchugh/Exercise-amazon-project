99fb1ff27530a42536e00c807c279d9899445227
realankitchugh <ankitchughpro@gmail.com>
2024-09-13 16:32:32 +0530

    Add quantity selection option

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -24,7 +24,7 @@ products.forEach((product) => {
       </div>
 
       <div class="product-quantity-container">
-        <select>
+        <select class="js-quantity-selector-${product.id}">
           <option selected value="1">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
@@ -57,6 +57,8 @@ document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
     button.addEventListener('click', () => {
       const productId = button.dataset.productId;
+      const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
+      console.log(quantitySelector.value);
       let matchingItem;
       cart.forEach((cartItem) => {
         if(cartItem.productId === productId){
@@ -65,12 +67,12 @@ document.querySelectorAll('.js-add-to-cart')
       });
       
       if(matchingItem){
-        matchingItem.quantity+=1;
+        matchingItem.quantity+=Number(quantitySelector.value);
       }
       else{
         cart.push({
           productId: productId,
-          quantity: 1
+          quantity: Number(quantitySelector.value)
         });
       }
       let finalQuantity=0;
