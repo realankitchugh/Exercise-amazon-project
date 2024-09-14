7cbf10d7fddc86cf73be1636424508bffade13e0
realankitchugh <ankitchughpro@gmail.com>
2024-09-14 23:43:26 +0530

    delete products from cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart, removeFromCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './Utils/money.js';
 let cartSummaryHTML='';
@@ -36,7 +36,7 @@ cart.forEach((cartItem, index) => {
           <span class="update-quantity-link link-primary">
             Update
           </span>
-          <span class="delete-quantity-link link-primary">
+          <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
             Delete
           </span>
         </div>
@@ -93,7 +93,25 @@ cart.forEach((cartItem, index) => {
 document.querySelector('.js-order-summary')
   .innerHTML=cartSummaryHTML;
 
-
+document.querySelectorAll('.js-delete-link')
+  .forEach((link) => {
+    link.addEventListener('click', () => {
+      console.log('delete');
+      const productId = link.dataset.productId;
+      console.log(productId);
+      removeFromCart(productId);
+      console.log(cart);
+      // let cartItemIndex;
+      // cart.forEach((cartItem, index) => {
+      //   if(cartItem.productId === productId){
+      //     cartItemIndex = index;
+      //   }
+      // });
+      // console.log(cartItemIndex);
+      // cart.splice(cartItemIndex, 1);
+      // console.log(cart);
+    });
+  });
 
 
 
