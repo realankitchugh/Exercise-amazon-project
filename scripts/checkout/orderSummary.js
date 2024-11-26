d12a9705d8808f26f3f6bc355fe925f9394444a2
realankitchugh <ankitchughpro@gmail.com>
2024-11-26 19:15:40 +0530

    Make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,6 +3,7 @@ import {products, getProduct} from '../../data/products.js';
 import formatCurrency from '../Utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import { deliveryOptions, getDeliveryOption } from '../../data/deliveryOptions.js';
+import { renderPaymentSummary } from './paymentSummary.js';
 export function renderOrderSummary(){
 
   let cartSummaryHTML='';
@@ -135,6 +136,7 @@ export function renderOrderSummary(){
         updateCartQuantity();
         const container=document.querySelector(`.js-cart-item-container-${productId}`);
         container.remove();
+        renderPaymentSummary();
       });
     });
 
@@ -143,6 +145,7 @@ export function renderOrderSummary(){
       const {productId, deliveryOptionId} = elem.dataset;
       updateDeliveryOption(productId, deliveryOptionId);
       renderOrderSummary();
+      renderPaymentSummary();
     });
   });
 
